1237d4cd4f0ed24f99f935fe27c32075d9acea3b
Jose Luis <jluisvazquez.jv@gmail.com>
2020-06-16 15:41:44 -0500

    08 personalizando mensaje

--- js/registro.js
+++ js/registro.js
@@ -18,6 +18,7 @@ function registro(e){
     let passVal = pass.value;
 
     if(nombreVal == '' || emailVal == '' || userVal == '' || passVal == ''){
+        creaMensaje('Verifica tus campos', 'danger')
         return;
     }
 
@@ -35,6 +36,16 @@ function registro(e){
     user.value = '';
     pass.value = '';
 
-    console.log('usuario guardado')
+    creaMensaje('Usuario Registrado', 'success');
+}
 
+function creaMensaje(texto, tipo){
+    const nuevoElemento = document.createElement('div');
+    nuevoElemento.innerText = texto;
+    nuevoElemento.classList.add('alert', 'alert-' + tipo);
+    const divMensaje = document.getElementById('mensaje');
+    divMensaje.appendChild(nuevoElemento);
+    setTimeout(function(){
+        nuevoElemento.remove();
+    }, 2000);
 }
